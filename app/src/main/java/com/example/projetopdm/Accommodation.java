dec77c1c467c356b07df7bb4fd1c2e8b7e4404f0
Flavio-LP <68715389+Flavio-LP@users.noreply.github.com>
2021-10-26 13:25:26 -0300

    criação classes java para alguams telas, correção da criação de tabelas no sqlite, implementação do insert dos combustiveis

---
+++ app/src/main/java/com/example/projetopdm/Accommodation.java
@@ -0,0 +1,15 @@
+package com.example.projetopdm;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Accommodation extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accommodation);
+    }
+}
---
+++ app/src/main/java/com/example/projetopdm/Entretenimento.java
@@ -0,0 +1,15 @@
+package com.example.projetopdm;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Entretenimento extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entertainment);
+    }
+}
